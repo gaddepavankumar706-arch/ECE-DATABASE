1fa909370a8ce90bdb017f07f57c6d423f1ac915
gaddepavankumar706-arch <gaddepavankumar706@gmail.com>
2026-09-07 17:46:27 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB2f_fRyEzj3EunZgqIkNkkNBJvLAJ-3w8",
   authDomain: "ece-status-profile.firebaseapp.com",
@@ -17,6 +18,21 @@ const firebaseConfig = {
   measurementId: "G-R3F6X0L41S"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
+
+// Your app.js expects this export.
+const isConfigPlaceholder =
+  !firebaseConfig.apiKey ||
+  firebaseConfig.apiKey === "YOUR_API_KEY";
+
+export {
+  app,
+  auth,
+  db,
+  googleProvider,
+  isConfigPlaceholder
+};
